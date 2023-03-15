e68f834a81cad631185f7a5f30d590e7a1f81b95
xdkerx <779867650@qq.com>
2023-03-15 23:44:46 +0800

    feat: 增加discord oauth2登录

--- apps/authentication/apis.py
+++ apps/authentication/apis.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from apps.users.selectors import user_get_login_data
+from .service import exchange_code
+from django.shortcuts import redirect
+
+from ..users.models import User
 
 
 class LoginApi(APIView):
@@ -35,3 +39,30 @@ class LogoutApi(APIView):
     def post(self, request):
         logout(request)
         return Response()
+
+
+class DiscordOauth2LoginApi(APIView):
+    def get(self, request):
+        return Response({'discord_auth_url': 'https://discord.com/api/oauth2/authorize?client_id=&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Foauth2%2Flogin%2Fredirect&response_type=code&scope=identify'})
+
+
+class DiscordOauth2RedirectApi(APIView):
+    def get(self, request):
+        code = request.GET.get('code')
+        if code is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = exchange_code(code)
+        if user is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        discord_id = user.get('id')
+        # 查询用户是否存在
+        user = User.objects.filter(discord_id=discord_id).first()
+        if user is None:
+            # 返回标志，前端跳转到注册页面，注册附带discord_id
+            return Response({'status': 'register', 'discord_id': discord_id})
+        else:
+            # 登录
+            discord_user = authenticate(request, user=user)
+            login(request, discord_user)
+            # 重定向到用户页面
+            return redirect("/dashboard/")
---
+++ apps/authentication/service.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def exchange_code(code: str):
+    data = {
+        "client_id": "988715519010041866",
+        "client_secret": "u1S2kDo1-GmwHc5R-wsG_jxKJExRP6YA",
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": "http://localhost:8000/oauth2/login/redirect",
+        "scope": "identify"
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    response = requests.post("https://discord.com/api/oauth2/token", data=data, headers=headers)
+    print(response)
+    credentials = response.json()
+    access_token = credentials['access_token']
+    response = requests.get("https://discord.com/api/v6/users/@me", headers={
+        'Authorization': 'Bearer %s' % access_token
+    })
+    print(response)
+    user = response.json()
+    print(user)
+    id = user['id']
+    return user
