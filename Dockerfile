de71b3bee02518ec08daddd9438e88912f99d8ec
xdkerx <779867650@qq.com>
2023-03-16 03:16:10 +0000

    修改Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,14 @@ ARG PYTHON_VERSION=3.10
 FROM python:${PYTHON_VERSION}
 RUN mkdir /opt/mentos_shop_backend
 COPY . /opt/mentos_shop_backend
+COPY .env.example /opt/mentos_shop_backend/.env
 WORKDIR /opt/mentos_shop_backend
-RUN apt-get update -y && pip install -r requirements.txt
+RUN pip install -r requirements.txt
+RUN python manage.py makemigrations
+RUN python manage.py migrate
 ENV TZ="Asia/Shanghai" 
-COPY config/bot_service.ini /opt/
+COPY config/supervisord_mentos.conf /opt/
 COPY config/supervisord.conf /etc/supervisor/supervisord.conf
 EXPOSE 8000 8000
-CMD ["sh","/opt/bot_api/config/docker-entrypoint.sh"]
+VOLUME ["/opt/mentos_shop_backend", "/opt/mentos_shop_backend"]
+CMD ["sh","/opt/mentos_shop_backend/config/docker-entrypoint.sh"]
--- config/django/base.py
+++ config/django/base.py
@@ -15,6 +15,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from config.env import env
+
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -77,8 +80,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        
     }
 }
 
