9aa8a6c0f3ead6fc301be84483d793a95b05ed71
xdkerx <779867650@qq.com>
2023-03-16 06:42:30 +0000

    修改成mysql

--- config/django/base.py
+++ config/django/base.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
