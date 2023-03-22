4011c49439d37c63f8926625c8f44cebeb4d4c31
xdkerx <779867650@qq.com>
2023-03-22 08:10:27 +0000

    fix mock.py

--- mocks/create_users.py
+++ mocks/create_users.py
@@ -3,6 +3,9 @@
 import random
 import string
 from faker import Faker
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from apps.users.models import User
 
 fake = Faker(locale='en_US')
