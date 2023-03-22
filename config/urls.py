c861de827409c3a69769e064e955691a43c9f165
xdkerx <779867650@qq.com>
2023-03-22 23:47:01 +0800

    fix: 静态文件bug

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls')),
+
 ]
+
+urlpatterns += staticfiles_urlpatterns()
