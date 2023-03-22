1f7b46ad92d18f9abea1633785df764de302662b
xdkerx <779867650@qq.com>
2023-03-22 11:07:49 +0000

    fix

--- apps/proxy_server/apps.py
+++ apps/proxy_server/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ProxyServerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'proxy_server'
+    name = 'apps.proxy_server'
