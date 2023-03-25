46d6479e146cc4359cce109355ec023f2784f3ad
xdkerx <779867650@qq.com>
2023-03-26 00:23:43 +0800

    fix: 删除test docker 移除8000端口暴露

--- redeploy.sh
+++ redeploy.sh
@@ -4,6 +4,8 @@ git pull
 cp -r config/* /opt/mentos_shop_backend/config/
 cp -r dist /opt/mentos_shop_backend/
 # no-cache is used to force docker to rebuild the image
+
 docker-compose build --no-cache
+docker-compose down
 docker-compose up -d
 
