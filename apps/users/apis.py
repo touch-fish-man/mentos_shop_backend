f5522c07c914f5be7e2c4aa8c7498390fa3fb2af
xdkerx <779867650@qq.com>
2023-03-26 02:31:37 +0800

    fix:  重置密码uid错误

--- apps/users/apis.py
+++ apps/users/apis.py
@@ -100,8 +100,9 @@ class UserApi(ComModelViewSet):
     def change_password(self, request, *args, **kwargs):
 
         user = request.user
-        if user.is_superuser or user.username == "AnonymousUser":
-            instance = User.objects.filter(uid=kwargs.get("pk")).first()
+
+        if user.is_superuser:
+            instance = User.objects.filter(id=kwargs.get("pk")).first()
             password = request.data.get('password')
             instance.password = make_password(password)
             instance.save()
