92474a17ccdce3aa727be6a43bcc095ab8af7536
xdkerx <779867650@qq.com>
2023-03-26 01:08:36 +0800

    fix: 删除旧前端文件

--- redeploy.sh
+++ redeploy.sh
@@ -2,8 +2,11 @@
 # This script is used to redeploy the application
 git pull
 cp -r config/* /opt/mentos_shop_backend/config/
+rm -rf /opt/mentos_shop_backend/dist/*
 mkdir -p /opt/mentos_shop_backend/dist
-unzip dist/dist.zip -d /opt/mentos_shop_backend/dist
+# -f is used to force overwrite
+
+unzip dist/dist.zip -d /opt/mentos_shop_backend/dist/ -f
 
 # no-cache is used to force docker to rebuild the image
 
