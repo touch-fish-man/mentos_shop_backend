31dcb8bbf5cca8fc9d89c4419a081c4ccb83ae7d
xdkerx <779867650@qq.com>
2023-03-26 21:34:02 +0800

    fix: add dev

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # This script is used to deploy the application locally
+conda active py38
 cp -r .env.sample config/.env
 pip install -r requirements.txt
 python manage.py reset_db --noinput
 python del_migrations.py
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
 python mocks/mock_data.py
-python manage.py runserver
+nohup  /root/anaconda3/envs/py38/bin/python manage.py runserver 0.0.0.0:4000 &
