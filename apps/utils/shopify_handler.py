e5e322ded840955cdda814e9028c1c34c1a6ec25
xdkerx <779867650@qq.com>
2023-03-28 23:27:04 +0800

    fix: 增加付款链接生成  build

--- apps/utils/shopify_handler.py
+++ apps/utils/shopify_handler.py
@@ -61,6 +61,29 @@ class ShopifyClient:
 
     def get_shop(self):
         return shopify.Shop.current()
+
+    def generate_cart_url(self,data):
+        base_url = self.shop_url+"/cart/clear?return_to=/cart/"
+
+        cart_quantity_pairs = data.get("cart_quantity_pairs")
+        email = data.get("email")
+        note = data.get("note")
+        discount = data.get("discount")
+        ref = data.get("ref")
+
+        # 将cart_id和quantity分别存储在两个列表中
+        cart_ids = []
+        quantities = []
+        for pair in cart_quantity_pairs:
+            pair_split = pair.split(":")
+            cart_ids.append(pair_split[0])
+            quantities.append(pair_split[1])
+
+        # 将多个cart_id和quantity拼接成字符串，用逗号隔开
+        cart_quantity_str = ",".join([cart_ids[i] + ":" + quantities[i] for i in range(len(cart_ids))])
+
+        url = base_url + cart_quantity_str + "?checkout[email]=" + email + "&note=" + note + "&discount=" + discount + "&ref=" + ref
+        return url
     def create_product(self, **kwargs):
         return shopify.Product.create(**kwargs)
 
