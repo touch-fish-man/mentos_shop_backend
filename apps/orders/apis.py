0f2c95e14cf55df04be9dc04e4d2b0c71264f0de
Internfivesixseven <1071697649@qq.com>
2023-04-01 19:48:52 +0800

    去除误传信息

--- apps/orders/apis.py
+++ apps/orders/apis.py
@@ -2,23 +2,19 @@ import datetime
 
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import action
-import pytz
 
-from apps.core.json_response import SuccessResponse,ErrorResponse,LimitOffsetResponse
+from apps.core.json_response import SuccessResponse,ErrorResponse
 from apps.core.viewsets import ComModelViewSet
 from apps.orders.models import Orders
 from apps.orders.serializers import OrdersSerializer, OrdersCreateSerializer, OrdersUpdateSerializer, \
-    OrdersStatusSerializer, ProxyListSerializer,UserOrdersSerializer,UserProxyListSerializer
+    OrdersStatusSerializer, ProxyListSerializer
 from apps.proxy_server.models import ProxyList
-from django.forms.models import model_to_dict
-from django.utils import timezone
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
+
 from .tasks import add
-from datetime import datetime, timedelta
-from celery.result import AsyncResult
-from django.utils.timezone import get_current_timezone
+from datetime import datetime
+
 
 
 
@@ -123,83 +119,3 @@ class OrdersApi(ComModelViewSet):
         serializer = self.get_serializer(instance)
         return SuccessResponse(data={"order": serializer.data, "proxy_list": proxy_list}, msg="获取成功")
     
-
-
-
-class UserOrdersApi(ComModelViewSet):
-    """
-    用户订单接口
-    list:获取订单列表
-    create:创建订单
-    retrieve:获取订单详情
-    update:更新订单
-    destroy:删除订单
-    get_status:获取订单状态
-    """
-    serializer_class = UserOrdersSerializer
-    # create_serializer_class = OrdersCreateSerializer
-    # update_serializer_class = OrdersUpdateSerializer
-    # get_status_serializer_class = OrdersStatusSerializer
-
-    def list(self, request, *args, **kwargs):
-        queryset = Orders.objects.filter(uid=request.user.uid,product_type=request.GET.get('product_type'))
-        data = []
-        for i in queryset:
-            dic1 = model_to_dict(i)
-            d1 = datetime.datetime.now().replace(tzinfo=datetime.timezone.utc)
-            d2 = i.expired_at.replace(tzinfo=datetime.timezone.utc)
-            dic1['days_remain']=(d2-d1).days
-            data.append(dic1)
-        return LimitOffsetResponse(data=data, msg="获取成功")
-    
-    @action(methods=['get'], detail=False, url_path='order_id', url_name='order_id')
-    def retrieve_order(self, request, *args, **kwargs):
-        queryset = Orders.objects.filter(id=request.GET.get('id'))
-        proxy = ProxyList.objects.filter(order_id=request.GET.get('id'))
-        proxy_list = []
-        if proxy.exists():
-            proxy_data = proxy.all()
-            serializer = UserProxyListSerializer(proxy_data, many=True)
-            proxy_list = serializer.data
-        serializer = self.get_serializer(queryset, many=True)
-        return SuccessResponse(data={"order": serializer.data, "proxy_list": proxy_list}, msg="获取成功")
-    
-    @action(methods=['delete'], detail=True)
-    def delete(self, request, *args, **kwargs):
-        queryset = Orders.objects.filter(id=request.GET.get('id'), uid = request.user.uid)
-        self.perform_destroy(queryset)
-        proxylist = ProxyList.objects.filter(order_id=request.GET.get('id'))
-        proxylist.delete()
-        return SuccessResponse(msg="删除成功")
-
-
-
-
-class EmailView(APIView):
-    def post(self, request):
-        print("第一次。")
-        a = request.data['a']
-        b = request.data['b']
-        current_time = timezone.now().astimezone(pytz.utc)
-        eta_time = current_time + timedelta(seconds=5)
-        result = add.apply_async((2,2),eta=eta_time)
-        task_id = result.id
-        print(result.status)
-        print(task_id)
-        # 异步调用send_email任务
-        # c = add.delay(a, b)
-        
-        return Response(data={'message': 'success','task_id': task_id})
-
-class VerifyView(APIView):
-    def post(self, request):
-        print("第二次。")
-        task_id = request.data['task_id']
-        print(task_id)
-        result = AsyncResult(task_id)
-        print(result.status)
-        print(result.ready())
-        # 异步调用send_email任务
-        # c = add.delay(a, b)
-        
-        return Response(data={'message': 'success','result': result.ready()})
--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
-from .apis import OrdersApi,UserOrdersApi
+from .apis import OrdersApi
 from rest_framework import routers
-from .apis import EmailView,VerifyView
 
 router = routers.DefaultRouter()
-router.register(r'order', OrdersApi, basename='orders')
-router.register(r'userorders',UserOrdersApi,basename="userorders")
+router.register(r'', OrdersApi, basename='orders')
+
 
 
 
 
 urlpatterns = [
-    path("test/", EmailView.as_view(), name="aaa"),
-    path("verify/", VerifyView.as_view(), name="bbb"),
 ]
 urlpatterns += router.urls
