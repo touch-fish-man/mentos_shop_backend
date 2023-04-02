9e75a7a9633d08900cb5806a23dae59233ce1ecd
xdkerx <779867650@qq.com>
2023-04-02 19:59:29 +0800

    fix: 修改bug [build docker] [build dev]

--- apps/rewards/apis.py
+++ apps/rewards/apis.py
@@ -40,12 +40,15 @@ class GiftCardViewSet(ComModelViewSet):
         获取礼品卡基本信息
         """
         queryset = self.get_queryset()
-        giftcard_amount_list = queryset.values_list('amount', flat=True).distinct()
+        giftcard_amount_list = queryset.values_list('mount', flat=True).distinct()
         # 查询不同金额礼品卡对应的point
-        giftcard_amount_point_dict = {}
-        for amount in giftcard_amount_list:
-            giftcard_amount_point_dict[amount] = queryset.filter(amount=amount).first().point
-        return SuccessResponse(data=giftcard_amount_point_dict)
+        data_list = []
+        for mount in giftcard_amount_list:
+            giftcard_amount_point_dict = {}
+            giftcard_amount_point_dict["mount"] = mount
+            giftcard_amount_point_dict["point"] = queryset.filter(mount=mount).first().point
+            data_list.append(giftcard_amount_point_dict)
+        return SuccessResponse(data=data_list, msg="获取成功")
 
 class LevelCodeViewSet(ComModelViewSet):
     """
