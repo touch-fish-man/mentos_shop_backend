e77cf04c5412ef333957d6e105549e009ca6c462
xdkerx <779867650@qq.com>
2023-04-06 11:20:42 +0000

    fix mock脚本时间过长 导致action超时

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -7,7 +7,7 @@ export DJANGO_ENV=dev
 /root/anaconda3/envs/py38/bin/python del_migrations.py
 /root/anaconda3/envs/py38/bin/python manage.py makemigrations
 /root/anaconda3/envs/py38/bin/python manage.py migrate
-/root/anaconda3/envs/py38/bin/python mocks/mock_data.py
+
 # kill the process running on port 4000
 kill -9 $(lsof -t -i:4000)
 # 删除已经存在的django screen
@@ -15,5 +15,5 @@ screen -X -S django quit
 # 创建django screen
 screen -dmS django
 # 在django screen中运行命令
-screen -S django -p 0 -X stuff "export DJANGO_ENV=dev && nohup  /root/anaconda3/envs/py38/bin/python manage.py runserver 0.0.0.0:4000 &";
+screen -S django -p 0 -X stuff "/root/anaconda3/envs/py38/bin/python mocks/mock_data.py && export DJANGO_ENV=dev && nohup  /root/anaconda3/envs/py38/bin/python manage.py runserver 0.0.0.0:4000 &";
 screen -S django -p 0 -X stuff $'\n';
