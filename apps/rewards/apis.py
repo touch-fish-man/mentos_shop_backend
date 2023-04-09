3a4088511ee197a0e2315be74d9e2f9182728c40
xdkerx <779867650@qq.com>
2023-04-09 23:18:38 +0800

    feat 礼品卡兑换接口 [build docker] [build dev]

--- apps/rewards/apis.py
+++ apps/rewards/apis.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import action
 
-from apps.core.json_response import SuccessResponse,ErrorResponse
+from apps.core.json_response import SuccessResponse, ErrorResponse
 from apps.core.viewsets import ComModelViewSet
 from apps.rewards.models import CouponCode, PointRecord, GiftCard, LevelCode
-from apps.rewards.serializers import CouponCodeSerializer, PointRecordSerializer, GiftCardSerializer,LevelCodeSerializer
+from apps.rewards.serializers import CouponCodeSerializer, PointRecordSerializer, GiftCardSerializer, \
+    LevelCodeSerializer
 from rest_framework.views import APIView
 
 
@@ -18,12 +19,12 @@ class CouponCodeViewSet(ComModelViewSet):
     """
     queryset = CouponCode.objects.all()
     serializer_class = CouponCodeSerializer
+
     def list(self, request, *args, **kwargs):
         # 获取当前用户优惠码
-        user=request.user
+        user = request.user
         if user.is_authenticated:
             if user.is_superuser:
-
                 # 管理员获取所有优惠码
                 return super().list(request, *args, **kwargs)
             # 普通用户获取自己的优惠码
@@ -32,9 +33,10 @@ class CouponCodeViewSet(ComModelViewSet):
             return super().list(request, *args, **kwargs)
         else:
             return ErrorResponse(msg="请先登录")
+
     def create(self, request, *args, **kwargs):
         # 创建优惠码
-        user=request.user
+        user = request.user
         if user.is_authenticated:
             if user.is_superuser:
                 # 管理员创建优惠码
@@ -43,9 +45,10 @@ class CouponCodeViewSet(ComModelViewSet):
                 return ErrorResponse(msg="没有权限")
         else:
             return ErrorResponse(msg="请先登录")
+
     def update(self, request, *args, **kwargs):
         # 更新优惠码
-        user=request.user
+        user = request.user
         if user.is_authenticated:
             if user.is_superuser:
                 # 管理员更新优惠码
@@ -54,9 +57,10 @@ class CouponCodeViewSet(ComModelViewSet):
                 return ErrorResponse(msg="没有权限")
         else:
             return ErrorResponse(msg="请先登录")
+
     def destroy(self, request, *args, **kwargs):
         # 删除优惠码
-        user=request.user
+        user = request.user
         if user.is_authenticated:
             if user.is_superuser:
                 # 管理员删除优惠码
@@ -65,9 +69,10 @@ class CouponCodeViewSet(ComModelViewSet):
                 return ErrorResponse(msg="没有权限")
         else:
             return ErrorResponse(msg="请先登录")
+
     def retrieve(self, request, *args, **kwargs):
         # 获取优惠码详情
-        user=request.user
+        user = request.user
         if user.is_authenticated:
             if user.is_superuser:
                 # 管理员获取优惠码详情
@@ -78,7 +83,6 @@ class CouponCodeViewSet(ComModelViewSet):
             return ErrorResponse(msg="请先登录")
 
 
-
 class PointRecordViewSet(ComModelViewSet):
     """
     积分变动记录
@@ -86,9 +90,10 @@ class PointRecordViewSet(ComModelViewSet):
     """
     queryset = PointRecord.objects.all()
     serializer_class = PointRecordSerializer
+
     def list(self, request, *args, **kwargs):
         # 获取当前用户积分变动记录
-        user=request.user
+        user = request.user
         if user.is_authenticated:
             self.queryset = self.queryset.filter(uid=user.id)
             return super().list(request, *args, **kwargs)
@@ -109,6 +114,7 @@ class GiftCardViewSet(ComModelViewSet):
     queryset = GiftCard.objects.all()
     serializer_class = GiftCardSerializer
     search_fields = ('code')
+
     @action(methods=['get'], detail=False, url_path='base-info', url_name='base-info')
     def base_info(self, request, *args, **kwargs):
         """
@@ -124,6 +130,39 @@ class GiftCardViewSet(ComModelViewSet):
             giftcard_amount_point_dict["point"] = queryset.filter(mount=mount).first().point
             data_list.append(giftcard_amount_point_dict)
         return SuccessResponse(data=data_list, msg="获取成功")
+
+    @action(methods=['post'], detail=False, url_path='exchange', url_name='exchange')
+    def exchange(self, request, *args, **kwargs):
+        """
+        兑换礼品卡
+        """
+        exchange_mount = request.data.get("mount")
+        user = request.user
+        if user.is_authenticated:
+            giftcards = GiftCard.objects.filter(mount=exchange_mount, is_used=False).all()
+            if giftcards:
+                if user.point < giftcards.first().point:
+                    return ErrorResponse(msg="积分不足")
+                giftcard = giftcards.first()
+                giftcard.is_exchanged = True
+                giftcard.save()
+                # 扣除用户积分
+                user.point -= giftcard.point
+                user.save()
+                # 创建积分变动记录
+                PointRecord.objects.create(uid=user.id, point=-giftcard.point,
+                                           reason=PointRecord.REASON_DICT["exchange"])
+                # 给用户增加礼品卡
+                CouponCode.objects.create(holder_uid=user.id, code=giftcard.code, discount=giftcard.mount,
+                                          is_used=False, code_type=CouponCode.CODE_TYPE_DICT_REVERSE["giftcard"],
+                                          holder_username=user.username)
+                return SuccessResponse(data=giftcard, msg="兑换成功")
+            else:
+                return ErrorResponse(msg="礼品卡已兑换完")
+
+        else:
+            return ErrorResponse(msg="请先登录")
+
 
 class LevelCodeViewSet(ComModelViewSet):
     """
