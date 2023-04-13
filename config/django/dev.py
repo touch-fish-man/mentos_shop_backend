2950b2a6cf3b58761b6160f832eeca1a43e0042c
xdkerx <779867650@qq.com>
2023-04-13 10:15:30 +0000

    增加pg docker

--- config/django/dev.py
+++ config/django/dev.py
@@ -1,6 +1,7 @@
 from .base import *
 DEBUG = True
-DATABASES["default"]["NAME"] = "mentos_dev"
+DATABASES["default"]["HOST"] = "13.231.170.92"
+DATABASES["default"]["PASSWORD"] = "N6jvHXYf4zn8sChw"
 CELERY_BROKER_BACKEND = "memory"
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
