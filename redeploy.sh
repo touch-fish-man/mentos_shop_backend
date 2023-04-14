1b9279b312e39206260cf6654de7b891967c9caa
xdkerx <779867650@qq.com>
2023-04-14 07:38:34 +0000

    fix

--- redeploy.sh
+++ redeploy.sh
@@ -11,5 +11,5 @@ unzip dist/dist.zip -d /opt/mentos_shop_backend/dist/
 # no-cache is used to force docker to rebuild the image
 
 docker-compose build --no-cache
-docker-compose down --remove-orphans
+docker-compose down
 docker-compose up -d
