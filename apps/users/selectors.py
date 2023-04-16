370f4a46ff2a5efe2bc7ece86037be85dd41c160
xdkerx <779867650@qq.com>
2023-04-17 00:32:09 +0800

    fix:bug [build docker] [build dev]

--- apps/users/selectors.py
+++ apps/users/selectors.py
@@ -11,8 +11,8 @@ def user_get_login_data(*, user: User):
         "username": user.username,
         "is_active": user.is_active,
         "is_superuser": user.is_superuser,
-        "discord_id":user.discord_id if len(user.discord_id) else None,
-        "discord_name": user.discord_name if len(user.discord_name) else None,
+        "discord_id":user.discord_id if user.discord_id else None,
+        "discord_name": user.discord_name if user.discord_name else None,
         "level_points": user.level_points,
         "reward_points": user.reward_points,
 
