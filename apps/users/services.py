690e87e60ed84069b9770b6bf5b2109822649cb1
xdkerx <779867650@qq.com>
2023-04-18 08:30:05 +0000

    fix 邮件验证时区问题

--- apps/users/services.py
+++ apps/users/services.py
@@ -28,9 +28,10 @@ def check_email_code(email, email_code_id, email_code, delete=False):
         return code_item.first()
     if code_item.exists():
         db_code = code_item.first()
-        # if db_code.create_time + settings.EMAIL_CODE_EXPIRE < datetime.datetime.now():
-        if db_code.created_at + datetime.timedelta(minutes=settings.EMAIL_CODE_EXPIRE) < datetime.datetime.now().replace(tzinfo=pytz.timezone('UTC')):
-            raise CustomValidationError("验证码已过期")
+        # 时区统一为UTC
+        code_created_at = db_code.created_at.replace(tzinfo=pytz.timezone('UTC'))
+        if code_created_at + datetime.timedelta(minutes=settings.EMAIL_CODE_EXPIRE // 60) < datetime.datetime.now().replace(tzinfo=pytz.timezone('UTC')):
+            raise CustomValidationError("Email code expired, please try again")
         if db_code.code.lower() == email_code.lower() or settings.DEBUG:
             if delete:
                 db_code.delete()
@@ -38,9 +39,9 @@ def check_email_code(email, email_code_id, email_code, delete=False):
                 return db_code
             return True
         else:
-            raise CustomValidationError("验证码错误")
+            raise CustomValidationError("Email code error, please try again")
     else:
-        raise CustomValidationError("验证码错误")
+        raise CustomValidationError("Email code error, please try again")
 
 
 def check_verify_id(email, verify_id):
@@ -64,18 +65,13 @@ def send_email_code(email, email_template):
             db_code.delete()
         else:
             return db_code.id
-    code = generate_code(4)
+    code = generate_code(6)
     email_template=settings.EMAIL_TEMPLATES.get(email_template)
     subject = email_template.get('subject')
     html_message = email_template.get('html').replace('{{code}}', code).replace("{{expire_time}}",str(
                                                                                 int(settings.EMAIL_CODE_EXPIRE / 60)))
     from_email = email_template.get('from_email')
-    if settings.EMAIL_METHOD == 'sendgrid':
-        send_success = send_via_sendgrid(email, subject, from_email, html_message)
-    elif settings.EMAIL_METHOD == 'mailgun':
-        send_success = send_email_via_mailgun(email, subject, from_email, html_message)
-    else:
-        send_success = send_mail(subject, "", from_email, [email], html_message=html_message)
+    send_success=send_email_api(email, subject, from_email, html_message)
     if send_success:
         code_obj = Code.objects.create(email=email, code=code)
         code_obj.save()
@@ -84,7 +80,14 @@ def send_email_code(email, email_template):
     else:
         ret = None
     return ret
-
+def send_email_api(email, subject, from_email, html_message):
+    if settings.EMAIL_METHOD == 'sendgrid':
+        send_success = send_via_sendgrid(email, subject, from_email, html_message)
+    elif settings.EMAIL_METHOD == 'mailgun':
+        send_success = send_email_via_mailgun(email, subject, from_email, html_message)
+    else:
+        send_success = send_mail(subject, "", from_email, [email], html_message=html_message)
+    return send_success
 
 def send_email_via_mailgun(email, subject, from_email, html_message):
     url = "https://api.mailgun.net/v3/{}/messages".format(settings.MAILGUN_SENDER_DOMAIN)
