463344b1d4f798e1682c58395815efd1f84c2f55
xdkerx <779867650@qq.com>
2023-04-19 07:29:34 +0000

    fix mock [build docker]

--- mocks/servers.py
+++ mocks/servers.py
@@ -13,26 +13,30 @@ from rich.console import Console
 console = Console()
 
 fake = Faker(locale='zh_CN')
-
+cidr_list=['217.144.59.0/24',
+       '217.144.60.0/24',
+       '213.181.216.0/24',
+       '213.181.217.0/24',
+       '213.181.218.0/24',
+       '213.181.221.0/24',
+       '113.203.224.0/24',
+       '113.203.225.0/24',
+       '113.203.223.0/24']
 def main():
     with console.status("[bold green]Generating proxy servers...") as status:
         ServerGroup.objects.all().delete()
         Server.objects.all().delete()
         Cidr.objects.all().delete()
         print("Generating proxy servers...")
-        for i in range(10):
+        for i in range(5):
             name = 'test proxy server {}'.format(i)
             description = fake.sentence()
             ip = '112.75.252.4'
             server = Server.objects.create(name=name, description=description, ip=ip)
-            cidr = fake.ipv4(network=True)
-            while int(cidr.split('/')[1])>29:
-                cidr = fake.ipv4(network=True)
-            if int(cidr.split('/')[1])<25:
-                cidr = cidr.split('/')[0]+'/'+str(random.randint(25, 29))
-            ip_count = cidr_ip_count(cidr)
-            cidr = Cidr.objects.create(cidr=cidr, ip_count=ip_count)
-            server.cidrs.add(cidr)
+            for cidr in cidr_list:
+                ip_count = cidr_ip_count(cidr)
+                cidr = Cidr.objects.create(cidr=cidr, ip_count=ip_count)
+                server.cidrs.add(cidr)
 
         for i in range(10):
             name = 'test proxy server group {}'.format(i)
