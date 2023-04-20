c04278c017a3e50eb0b236642b931f8f336013e7
xdkerx <779867650@qq.com>
2023-04-20 09:04:56 +0000

    修改redis docker

--- config/django/local.py
+++ config/django/local.py
@@ -5,7 +5,6 @@ DATABASES["default"]["PORT"] = "5432"
 CELERY_BROKER_BACKEND = "memory"
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
-
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
