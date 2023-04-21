067f4cf6d31a99ccd97620817a0fed18102fc0c7
xdkerx <779867650@qq.com>
2023-04-21 03:22:13 +0000

    feat:geofeed

--- apps/site_settings/apis.py
+++ apps/site_settings/apis.py
@@ -8,23 +8,32 @@ from apps.core.json_response import SuccessResponse,ErrorResponse
 from apps.site_settings.services import save_site_settings,change_site_settings,get_site_setting
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class SiteSettingsApi(APIView,LoginRequiredMixin):
+class SiteSettingsApi(APIView, LoginRequiredMixin):
     """
     站点设置
     """
     permission_classes = [IsSuperUser]
-    def post(self,request):
-        BASE_DIR = Path(__file__).resolve().parent.parent.parent
-        save_site_settings(data=request.data,file=os.path.join(BASE_DIR, "config",".env"))
+
+    def post(self, request):
+        BASE_DIR = settings.BASE_DIR
+        if request.data.get("geo_feed"):
+            with open("/opt/geofeed/geofeed.csv", "w") as f:
+                f.write(request.data.get("geofeed"))
+        save_site_settings(data=request.data, file=os.path.join(BASE_DIR, "config", ".env"))
         revoke = change_site_settings()
         # 验证是否修改成功，失败则撤回修改
         if not revoke:
             data = get_site_setting()
-            return SuccessResponse(msg="保存成功",data=data)
+            return SuccessResponse(msg="保存成功", data=data)
         else:
-            save_site_settings(data=revoke,file=os.path.join(BASE_DIR, "config",".env"))
-            return ErrorResponse(msg="保存失败",data={})
-    
-    def get(self,request):
+            save_site_settings(data=revoke, file=os.path.join(BASE_DIR, "config", ".env"))
+            return ErrorResponse(msg="保存失败", data={})
+
+    def get(self, request):
         data = get_site_setting()
-        return SuccessResponse(data=data,msg=("获取成功"))
+        try:
+            geofeed = open("/opt/geofeed/geofeed.csv", "r").read()
+        except:
+            geofeed = ""
+        data["geofeed"] = geofeed
+        return SuccessResponse(data=data, msg=("获取成功"))
