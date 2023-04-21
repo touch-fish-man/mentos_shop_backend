fa5a0785c855ddfd47f4b8d9436f3ec0abee32da
xdkerx <779867650@qq.com>
2023-04-21 15:40:25 +0000

    fix [build docker]

--- apps/proxy_server/tasks.py
+++ apps/proxy_server/tasks.py
@@ -10,11 +10,16 @@ def check_server_status():
     """
     servers = Server.objects.filter().all()
     for server in servers:
-        kaxy_client = KaxyClient(server.server_ip, server.server_port, server.server_username, server.server_password)
-        if kaxy_client.check_server_status():
-            server.server_status = 1
-            server.faild_count = 0
-        else:
+        kaxy_client = KaxyClient(server.ip)
+        try:
+            resp=kaxy_client.get_server_info()
+            if resp.status_code == 200:
+                server.server_status = 1
+                server.faild_count = 0
+            else:
+                server.server_status = 0
+                server.faild_count += 1
+        except Exception as e:
             server.server_status = 0
             server.faild_count += 1
         server.save()
