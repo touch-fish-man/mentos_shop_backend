344c5d71e6bc434b1cee7129e23cde41a49224a3
Internfivesixseven <1071697649@qq.com>
2023-04-21 19:54:01 +0800

    创建优惠码

--- apps/rewards/serializers.py
+++ apps/rewards/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from apps.rewards.models import CouponCode, PointRecord, GiftCard,LevelCode
 from apps.users.models import User
+from apps.core.serializers import CommonSerializer
 
 
 class CouponCodeSerializer(serializers.ModelSerializer):
@@ -10,20 +11,32 @@ class CouponCodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CouponCode
         fields = '__all__'
-class CouponCodeCreateSerializer(serializers.ModelSerializer):
+# class CouponCodeCreateSerializer(serializers.ModelSerializer):
+#     """
+#     创建优惠码
+#     """
+
+#     class Meta:
+#         model = CouponCode
+#         fields = ('code', 'discount', 'code_type', 'holder_username')
+
+#     def save(self, **kwargs):
+#         holder_username = kwargs.get("holder_username")
+#         holder_uid = User.objects.filter(user__username=holder_username).get("id")
+#         kwargs["holder_uid"] = holder_uid
+#         return super().save(**kwargs)
+class CouponCodeCreateSerializer(CommonSerializer):
     """
     创建优惠码
     """
+    def create(self, validated_data):
+        user = User.objects.filter(username=validated_data['holder_username']).first()
+        validated_data['holder_uid'] = user.id
+        return super().create(validated_data)
 
     class Meta:
         model = CouponCode
         fields = ('code', 'discount', 'code_type', 'holder_username')
-
-    def save(self, **kwargs):
-        holder_username = kwargs.get("holder_username")
-        holder_uid = User.objects.filter(user__username=holder_username).get("id")
-        kwargs["holder_uid"] = holder_uid
-        return super().save(**kwargs)
 
 class PointRecordSerializer(serializers.ModelSerializer):
     """
