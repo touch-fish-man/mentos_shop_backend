2f66d74543b1b9013d97310329bfed9279501d51
Internfivesixseven <1071697649@qq.com>
2023-04-22 14:38:09 +0800

    check_order_expired

--- apps/orders/tasks.py
+++ apps/orders/tasks.py
@@ -18,7 +18,7 @@ def precheck_order_expired(ceheck_days=3,send_email=True):
     """
     utc_now = datetime.datetime.now(tz=pytz.utc)
     utc_today = utc_now.date()
-    orders = Orders.objects.filter(order_status=1).values('expired_at','order_id','uid')
+    orders = Orders.objects.filter(order_status=4).values('expired_at','order_id','uid')
     for order in orders:
         precheck_day = (order['expired_at']-datetime.timedelta(days=ceheck_days)).date()
         if utc_today == precheck_day:
@@ -40,9 +40,28 @@ def check_order_expired():
     """
     定时检查db中订单状态，如果订单已过期，删除代理，每天检查一次，添加当天过期删除任务
     """
-    orders = Orders.objects.filter(order_status=3).values('id')
+    utc_now = datetime.datetime.now(tz=pytz.utc)
+    utc_today = utc_now.date()
+    orders = Orders.objects.all().values('id','expired_at')
     for order in orders:
-        delete_proxy_expired(order['id'])
+        expired_day = order['expired_at'].date()
+        if utc_today == expired_day and utc_now<order['expired_at']:
+            change_order.apply_async(args=[order['id']], eta=order['expired_at'])
+        elif utc_today == expired_day and utc_now>order['expired_at']:
+            change_order.apply_async(args=[order['id']], eta=utc_now+datetime.timedelta(minutes=5))
+
+@shared_task(name='change_order')
+def change_order(order_id):
+    Orders.objects.filter(id=order_id).update(order_status=3)
+    proxys = Proxy.objects.filter(order_id=order_id)
+    server_ip = proxys.first().server_ip
+    client = KaxyClient(server_ip)
+    for proxy in proxys:
+        if proxy.server_ip != server_ip:
+            server_ip = proxy.server_ip
+            client.del_user(proxy.username)
+            client = KaxyClient(server_ip)
+    proxys.delete()
 
 @shared_task(name='delete_proxy_expired')
 def delete_proxy_expired():
@@ -52,7 +71,7 @@ def delete_proxy_expired():
     del_user_dict = {}
     all_proxy=Proxy.objects.filter().all()
     for proxy in all_proxy:
-        if proxy.expired_at < datetime.datetime.now():
+        if proxy.expired_at < datetime.datetime.now(tz=pytz.utc):
             if proxy.server_ip not in del_user_dict:
                 del_user_dict[proxy.server_ip] = set()
             del_user_dict[proxy.server_ip].add(proxy.username)
