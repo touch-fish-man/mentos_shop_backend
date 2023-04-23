5ad1af010b347f630628698a7bcab06d25a71758
xdkerx <779867650@qq.com>
2023-04-23 01:53:39 +0000

    修改docker-compose [build docker]

--- config/django/test.py
+++ config/django/test.py
@@ -2,5 +2,5 @@ from .base import *
 DEBUG = True
 REDIS_PASSWORD = 'xB8U0Q6gyrMpRYA7'
 REDIS_HOST = '177.8.0.14'
-REDIS_HOST = '177.8.0.14'
+FRONTEND_URL='https://test.mentosproxy.com'
 CELERY_BROKER_URL = 'redis://:{}@{}/2'.format(REDIS_PASSWORD, REDIS_HOST)
