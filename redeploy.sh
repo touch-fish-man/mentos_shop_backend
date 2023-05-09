c9e3fc84123914c72757020c24a8397e49046a2b
xdkerx <779867650@qq.com>
2023-05-10 00:46:59 +0800

    update [build docker]

--- redeploy.sh
+++ redeploy.sh
@@ -5,8 +5,9 @@ cp -r config/* /opt/mentos_shop_backend/config/
 rm -rf /opt/mentos_shop_backend/dist/*
 mkdir -p /opt/mentos_shop_backend/dist
 # -f is used to force overwrite
-# wget https://zlp-1251420975.cos.accelerate.myqcloud.com/vue/dist.zip -O dist/dist.zip
+wget https://zlp-1251420975.cos.accelerate.myqcloud.com/vue/dist.zip -O dist/dist.zip
 unzip dist/dist.zip -d /opt/mentos_shop_backend/dist/
+rm -rf dist/dist.zip
 
 # no-cache is used to force docker to rebuild the image
 
