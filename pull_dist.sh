6d579588b67874889f7c7b6949d8ff134daf86b5
xdkerx <779867650@qq.com>
2023-05-14 04:26:23 +0800

    fix update sh

--- pull_dist.sh
+++ pull_dist.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-wget  https://zlp-1251420975.cos.accelerate.myqcloud.com/vue/dist.zip -O dist/dist.zip
+wget  https://zlp-1251420975.cos.accelerate.myqcloud.com/vue/dist.zip -O /tmp/dist.zip
+unzip /tmp/dist.zip -d /opt/mentos_shop_backend/dist/
+rm -rf /tmp/dist.zip
--- redeploy.sh
+++ redeploy.sh
@@ -2,12 +2,8 @@
 # This script is used to redeploy the application
 git pull
 cp -r config/* /opt/mentos_shop_backend/config/
-rm -rf /opt/mentos_shop_backend/dist/*
-mkdir -p /opt/mentos_shop_backend/dist
 # -f is used to force overwrite
-wget  https://zlp-1251420975.cos.accelerate.myqcloud.com/vue/dist.zip -O /tmp/dist.zip
-unzip /tmp/dist.zip -d /opt/mentos_shop_backend/dist/
-rm -rf /tmp/dist.zip
+sh pull_dist.sh
 
 # no-cache is used to force docker to rebuild the image
 
