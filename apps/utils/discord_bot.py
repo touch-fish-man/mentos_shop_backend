5caa8cb37db9c8176230bd97e4049f3c2110a899
xdkerx <779867650@qq.com>
2023-05-20 11:13:49 +0800

    fix  debug

--- apps/utils/discord_bot.py
+++ apps/utils/discord_bot.py
@@ -6,14 +6,15 @@ import asyncio
 import sys
 import django
 
-if os.environ.get('DJANGO_ENV'):
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    if os.path.exists(os.path.join(base_dir, 'config', 'django', os.environ.get('DJANGO_ENV') + '.py')):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django." + os.environ.get('DJANGO_ENV'))
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.local")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.local")
+# if os.environ.get('DJANGO_ENV'):
+#     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#     if os.path.exists(os.path.join(base_dir, 'config', 'django', os.environ.get('DJANGO_ENV') + '.py')):
+#         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django." + os.environ.get('DJANGO_ENV'))
+#     else:
+#         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.local")
+# else:
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.pord")
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 django.setup()
 from django.conf import settings
