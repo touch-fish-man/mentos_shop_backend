ab2a8c23a048692a552877e9a19412226728392d
xdkerx <779867650@qq.com>
2023-05-23 02:44:37 +0000

    uu

--- apps/utils/shopify_handler.py
+++ apps/utils/shopify_handler.py
@@ -11,7 +11,6 @@ import shopify
 
 if os.environ.get('DJANGO_ENV'):
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    print(base_dir)
     if os.path.exists(os.path.join(base_dir, 'config', 'django', os.environ.get('DJANGO_ENV') + '.py')):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django." + os.environ.get('DJANGO_ENV'))
     else:
