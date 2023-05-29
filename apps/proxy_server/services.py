a5fe9155c2bfe43d385b3cdf7c0e066c57f8816c
xdkerx <779867650@qq.com>
2023-05-29 07:02:49 +0000

    xxx

--- apps/proxy_server/services.py
+++ apps/proxy_server/services.py
@@ -1,3 +1,4 @@
+import json
 import time
 from datetime import datetime
 
@@ -39,5 +40,14 @@ def reset_proxy_fn(order_id, username, server_ip):
 
 def create_proxy_task(order_id, username, server_ip):
     # 创建一次性celery任务，立即执行，执行完毕后删除
-    task_name = 'reset_proxy_{}_{}'.format(order_id, username)
-    reset_proxy_fn.apply_async(args=[order_id, username, server_ip], task_id=task_name)
+    random_str = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    interval = IntervalSchedule(every=1, period=IntervalSchedule.SECONDS)
+    interval.save()
+    PeriodicTask.objects.get_or_create(
+        name=f'重置代理_{order_id}',
+        task='apps.proxy_server.services.reset_proxy_fn',
+        args=json.dumps([order_id, username, server_ip]),
+        interval=interval,
+        start_time=datetime.now(),
+        one_off=True,
+    )
