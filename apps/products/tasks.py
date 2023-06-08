fdeecbb36b503d4e90386dcf6e6a26d8993f00ab
xdkerx <779867650@qq.com>
2023-06-08 01:40:01 +0000

    update task

--- apps/products/tasks.py
+++ apps/products/tasks.py
@@ -1,16 +1,32 @@
 from apps.products.models import Variant, ProductTag, ProductTagRelation
 from celery import shared_task
 
+from apps.proxy_server.models import ProxyStock, Proxy
+
 
 @shared_task(bind=True, name='update_product_stock')
 def update_product_stock(*args, **kwargs):
     """
     更新商品库存
     """
-    
-    for v in Variant.objects.all():
-        v.update_stock()
-        print("更新库存", v.id)
+
+    for xxx in ProxyStock.objects.all():
+        ppp = Proxy.objects.filter(ip_stock_id=xxx.id).all()
+        if xxx.subnets:
+            subnets = xxx.subnets.split(",")
+            for p in ppp:
+                if p.subnet in subnets:
+                    subnets.remove(p.subnet)
+            subnets.sort()
+            cart_stock = len(subnets)
+            xxx.available_subnets = ",".join(subnets)
+            if xxx.cart_stock != cart_stock:
+                xxx.cart_stock = cart_stock
+                xxx.available_subnets = ",".join(subnets)
+                xxx.ip_stock = cart_stock * xxx.cart_step
+                xxx.save()
+    for x in Variant.objects.all():
+        x.save()
 
 
 def update_product_from_shopify():
