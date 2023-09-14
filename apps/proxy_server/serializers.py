b2b64c64e74d5add40fc2075f5ae73a30ca78140
xdkerx <779867650@qq.com>
2023-09-14 21:47:40 +0800

    fix 更新acl

--- apps/proxy_server/serializers.py
+++ apps/proxy_server/serializers.py
@@ -57,7 +57,6 @@ class AclGroupCreateSerializer(CommonSerializer):
         model = AclGroup
         fields = ('id', 'name', 'description', 'acls')
 class AclGroupUpdateSerializer(CommonSerializer):
-    acls = serializers.PrimaryKeyRelatedField(many=True, queryset=Acls.objects.all(), required=False)
     name = serializers.CharField(required=True,
                                  validators=[CustomUniqueValidator(AclGroup.objects.all(), message="acl组名称已存在")])
     description = serializers.CharField(required=True)
