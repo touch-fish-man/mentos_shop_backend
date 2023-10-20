8d2efd881a6d0e7f5a671a32e1cbd006c5c98fab
xdkerx <779867650@qq.com>
2023-10-20 06:04:25 +0000

    add proxy check order

--- apps/proxy_server/tasks.py
+++ apps/proxy_server/tasks.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import os
+import socket
 import threading
 from concurrent.futures import ThreadPoolExecutor
 
@@ -17,11 +18,15 @@ from django_celery_beat.models import PeriodicTask, IntervalSchedule
 from apps.orders.services import create_proxy_by_id
 from apps.proxy_server.models import Proxy
 from celery import shared_task
+from django.views.decorators.cache import cache_page
 
 from apps.utils.kaxy_handler import KaxyClient
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from django.core.cache import cache
+
+
 @shared_task(name='check_server_status')
 def check_server_status():
     """
@@ -64,7 +69,7 @@ def reset_proxy_fn(order_id, username, server_ip):
     for server_ip, username in server_ip_username:
         kaxy_client = KaxyClient(server_ip)
         kaxy_client.del_user(username)
-    re_create_ret, ret_proxy_list,msg = create_proxy_by_id(order_id)
+    re_create_ret, ret_proxy_list, msg = create_proxy_by_id(order_id)
     if re_create_ret:
         new_proxy = Proxy.objects.filter(username=username).all()
         for p in new_proxy:
@@ -96,12 +101,28 @@ def delete_proxy_by_id(id):
 lock = threading.Lock()
 
 
+@cache_page(60 * 60 * 2)
+def is_port_open(proxy_port):
+    """
+    获取端口状态
+    """
+    ip, port = proxy_port.split(':')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)  # 设置超时，例如1秒
+    try:
+        s.connect((ip, port))
+        s.close()
+        return True
+    except socket.error:
+        return False
+
+
 @shared_task(name='delete_user_from_server')
 def delete_user_from_server(server_ip, username, subnet, ip_stock_id):
     cache_key = 'del_user_{}_{}'.format(username, server_ip)
-    if Proxy.objects.filter(username=username,server_ip=server_ip).count() == 0:
+    if Proxy.objects.filter(username=username, server_ip=server_ip).count() == 0:
         logging.info('删除用户{}'.format(username))
-        server_obj= Server.objects.filter(ip=server_ip).first()
+        server_obj = Server.objects.filter(ip=server_ip).first()
         if server_obj:
             if server_obj.server_status == 0:
                 logging.info('服务器{}已经下线,不需要删除用户'.format(server_ip))
@@ -111,7 +132,7 @@ def delete_user_from_server(server_ip, username, subnet, ip_stock_id):
             if not cache.get(cache_key):
                 resp = kax_client.del_user(username)
                 try:
-                    if resp.json().get('status')==200:
+                    if resp.json().get('status') == 200:
                         kax_client.del_acl(username)
                         cache.set(cache_key, 1, timeout=30)
                     elif "User does not exist." in resp.json().get('message'):
@@ -123,7 +144,7 @@ def delete_user_from_server(server_ip, username, subnet, ip_stock_id):
             cache.set(cache_key, 1, timeout=30)
             logging.info('删除用户失败,可能服务器挂了')
     stock = ProxyStock.objects.filter(id=ip_stock_id).first()
-    redis_key='stock_opt_{}'.format(ip_stock_id)
+    redis_key = 'stock_opt_{}'.format(ip_stock_id)
     oid = 'stock_opt_{}'.format(subnet)
     # 归还子网,归还库存
     if stock:
@@ -158,18 +179,23 @@ def create_proxy_task(order_id, username, server_ip):
         expires=timezone.now() + datetime.timedelta(seconds=70)
     )
 
+
 def check_proxy(proxy, id):
     status = True
+    port_open = True
     try:
         proxies = {
             'http': f'http://{proxy}',
             'https': f'http://{proxy}'
         }
-        response = requests.get('https://checkip.amazonaws.com', proxies=proxies, timeout=5, verify=False)
-        if response.status_code == 200:
-            status = True
-        else:
-            status = False
+        ip_port = proxy.split('@')[1]
+        port_open = is_port_open(ip_port)
+        if port_open:
+            response = requests.get('https://checkip.amazonaws.com', proxies=proxies, timeout=5, verify=False)
+            if response.status_code == 200:
+                status = True
+            else:
+                status = False
     except Exception as e:
         status = False
     if not status:
@@ -185,18 +211,25 @@ def check_proxy(proxy, id):
                 status = False
         except Exception as e:
             status = False
+    if not port_open:
+        status = False
     proxy_obj = Proxy.objects.filter(id=id).first()
     if proxy_obj:
         proxy_obj.status = status
         proxy_obj.save()
-    return status
+    return proxy, status, id
+
+
 @shared_task(name='check_proxy_status')
-def check_proxy_status():
+def check_proxy_status(order_id=None):
     """
     检查代理状态,每4个小时检查一次
     """
     # 获取所有代理
-    proxies = list(Proxy.objects.all())
+    if order_id:
+        proxies = list(Proxy.objects.filter(order_id=order_id).all())
+    else:
+        proxies = list(Proxy.objects.all())
     # 获取所有状态为0的服务器IP
     offline_server_ips = set(Server.objects.filter(server_status=0).values_list('ip', flat=True))
 
@@ -216,12 +249,24 @@ def check_proxy_status():
     # 使用线程池并发检查代理状态
     with ThreadPoolExecutor(max_workers=10) as executor:
         results = executor.map(lambda x: check_proxy(x[0], x[1]), proxy_data)
+    ret_json = {}
+    faild_list = []
+    for proxy, status, id in results:
+        if not status:
+            faild_list.append((proxy, id))
+    ret_json['code'] = 200
+    ret_json['message'] = 'success'
+    ret_json['faild_list'] = faild_list
+    return ret_json
+
 
 
 @shared_task(name="cleanup_sessions")
 def cleanup():
     """Cleanup expired sessions by using Django management command."""
     management.call_command("clearsessions", verbosity=0)
+
+
 @shared_task(name='flush_access_log')
 def clear_access_log():
     """
