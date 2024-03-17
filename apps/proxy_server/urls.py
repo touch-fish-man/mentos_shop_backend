a7566ca80deb3886f2faec0dff885d0b88d5c4ce
xdkerx <779867650@qq.com>
2024-03-17 14:47:52 +0800

    update acl api

--- apps/proxy_server/urls.py
+++ apps/proxy_server/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AclsApi,ProxyServerApi,AclGroupApi,ServerGroupApi
+from .views import AclsApi,ProxyServerApi,AclGroupApi,ServerGroupApi,CidrApi
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -7,5 +7,6 @@ router.register(r'acls', AclsApi)
 router.register(r'servers', ProxyServerApi)
 router.register(r'acl-groups', AclGroupApi)
 router.register(r'server-groups', ServerGroupApi)
+router.register(r'cidrs', CidrApi)
 urlpatterns = []
 urlpatterns += router.urls
