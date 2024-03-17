ac5ccd66ec4508e79e7318bf608306aa6153ff59
xdkerx <779867650@qq.com>
2024-03-17 15:11:12 +0800

    update acl api

--- apps/proxy_server/models.py
+++ apps/proxy_server/models.py
@@ -154,7 +154,7 @@ class CidrAclThrough(BaseModel):
 class Cidr(BaseModel):
     cidr = models.CharField(max_length=255, blank=True, null=True, verbose_name='CIDR')
     ip_count = models.IntegerField(blank=True, null=True, verbose_name='IP数量')
-    available_acl = models.ManyToManyField('Acls', verbose_name='ACL', through='CidrAclThrough')
+    available_acl = models.ManyToManyField('Acls', verbose_name='可用ACL', through='CidrAclThrough')
 
     class Meta:
         db_table = 'cidr'
