c79d0d09119ff2c5545b283e42e6a0c02870b78e
xdkerx <779867650@qq.com>
2024-04-07 12:31:12 +0800

    add

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -185,7 +185,7 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ("id",
                   'product_name', 'product_desc', 'shopify_product_id', 'product_tags', 'product_collections',
                   'variants',
-                  'variant_options', "created_at", "lower_price", "active", "old_flag")
+                  'variant_options', "created_at", "lower_price", "active", "old_flag", "valid")
 
     def get_lower_price(self, obj):
         if obj.variants.exists():
