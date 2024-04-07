07af8a02f190baacf4fe65b0c9dbc1241d6ad04f
xdkerx <779867650@qq.com>
2024-04-07 11:20:01 +0800

    aaa

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -294,6 +294,8 @@ class ShopifyProductWebhookApi(APIView):
         # 验证签名
         if not verify_webhook(request):
             return ErrorResponse(data={}, msg="签名验证失败")
+        query = request.query_params.dict()
+        logging.info("shopify产品回调query:{}".format(json.dumps(query)))
         logging.info("shopify产品回调信息:{}".format(json.dumps(request.data)))
         from apps.orders.tasks import update_shopify_product
         update_shopify_product.delay()
