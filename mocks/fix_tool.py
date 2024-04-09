38cbc5cc7b93ae7b6a0ed8fe3b6c1f1d516df73f
xdkerx <779867650@qq.com>
2024-04-09 23:13:18 +0800

    fix:库存回收

--- mocks/fix_tool.py
+++ mocks/fix_tool.py
@@ -23,7 +23,7 @@ from apps.proxy_server.models import Proxy, ProxyStock, ServerGroup, Server, Acl
 from apps.orders.models import Orders
 from apps.products.models import Variant, ProductTag, ProductTagRelation
 from apps.utils.kaxy_handler import KaxyClient
-
+from apps.products.services import add_product_other
 
 def is_ip_in_network(ip_str, network_str):
     ip = ipaddress.ip_address(ip_str)
